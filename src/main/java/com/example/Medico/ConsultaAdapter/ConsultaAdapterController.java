b5a749a117c26920cb5ea46ae5f80d13432483cd
Lucas <lucasfm801@gmail.com>
2025-06-10 19:23:23 -0300

    Post adapter

--- src/main/java/com/example/Medico/ConsultaAdapter/ConsultaAdapterController.java
+++ src/main/java/com/example/Medico/ConsultaAdapter/ConsultaAdapterController.java
@@ -3,28 +3,19 @@ package com.example.Medico.ConsultaAdapter;
 import com.example.Medico.ConsultaAdapter.ConsultaExternaAdapter;
 import com.example.Medico.ConsultaAdapter.ConsultaExternaDTO;
 import com.example.Medico.dtos.ConsultaDTO;
+import com.example.Medico.services.ConsultaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/adapter-teste")
 public class ConsultaAdapterController {
 
-    @GetMapping
-    public ResponseEntity<ConsultaDTO> testarAdapter() {
 
-        ConsultaExternaDTO externaDTO = new ConsultaExternaDTO();
-        externaDTO.setIdConsulta(999L);
-        externaDTO.setIdPaciente(111L);
-        externaDTO.setIdMedico(222L);
-        externaDTO.setData("2025-06-04T14:30:00"); 
-        externaDTO.setDetalhes("Consulta simulada via Adapter");
-
-
-        ConsultaDTO consultaDTO = ConsultaExternaAdapter.adaptar(externaDTO);
-
-        return ResponseEntity.ok(consultaDTO);
+    @PostMapping
+    public ResponseEntity<ConsultaDTO> adaptarConsulta(@RequestBody ConsultaExternaDTO externaDTO) {
+        ConsultaDTO adaptado = ConsultaExternaAdapter.adaptar(externaDTO);
+        return ResponseEntity.ok(adaptado);
     }
 }
